60647200e9f89d8322882fe893d600218fd7ceaf
Giedrius77 <giedrius.zalionis@gmail.com>
2024-01-11 13:27:44 +0200

    teorija ir uzduotys 0501

---
+++ 05_projects_modules_and_imports/matematika.py
@@ -0,0 +1,25 @@
+
+def add(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero!"
+    
+def subtract(x, y):
+    return x - y
+
+
+class Obuolys:
+    
+    def daugyba(self, x, y):
+        return x * y
+    
+    def dalyba(self, x, y):
+        return x / y
+     
